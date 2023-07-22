c0750007f61d2f9605ea2551af19a3e12ab4ac14
Mualu Kianisalu <mualu.kianisalu@gmail.com>
2023-07-22 14:02:45 -0700

    My first commit

--- _printf.c
+++ _printf.c
@@ -1,57 +1,90 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "main.h"
 
+void cleanup(va_list args, buffer_t *output);
+int run_printf(const char *format, va_list args, buffer_t *output);
+int _printf(const char *format, ...);
+
+/**
+ * cleanup - Peforms cleanup operations for _printf.
+ * @args: A va_list of arguments provided to _printf.
+ * @output: A buffer_t struct.
+ */
+void cleanup(va_list args, buffer_t *output)
+{
+	va_end(args);
+	write(1, output->start, output->len);
+	free_buffer(output);
+}
+
+/**
+ * run_printf - Reads through the format string for _printf.
+ * @format: Character string to print - may contain directives.
+ * @output: A buffer_t struct containing a buffer.
+ * @args: A va_list of arguments.
+ *
+ * Return: The number of characters stored to output.
+ */
+int run_printf(const char *format, va_list args, buffer_t *output)
+{
+	int i, wid, prec, ret = 0;
+	char tmp;
+	unsigned char flags, len;
+	unsigned int (*f)(va_list, buffer_t *,
+			unsigned char, int, int, unsigned char);
+
+	for (i = 0; *(format + i); i++)
+	{
+		len = 0;
+		if (*(format + i) == '%')
+		{
+			tmp = 0;
+			flags = handle_flags(format + i + 1, &tmp);
+			wid = handle_width(args, format + i + tmp + 1, &tmp);
+			prec = handle_precision(args, format + i + tmp + 1,
+					&tmp);
+			len = handle_length(format + i + tmp + 1, &tmp);
+
+			f = handle_specifiers(format + i + tmp + 1);
+			if (f != NULL)
+			{
+				i += tmp + 1;
+				ret += f(args, output, flags, wid, prec, len);
+				continue;
+			}
+			else if (*(format + i + tmp + 1) == '\0')
+			{
+				ret = -1;
+				break;
+			}
+		}
+		ret += _memcpy(output, (format + i), 1);
+		i += (len != 0) ? 1 : 0;
+	}
+	cleanup(args, output);
+	return (ret);
+}
+
+/**
+ * _printf - Outputs a formatted string.
+ * @format: Character string to print - may contain directives.
+ *
+ * Return: The number of characters printed.
+ */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    va_start(args, format);
+	buffer_t *output;
+	va_list args;
+	int ret;
 
-    int count = 0;
-    char c;
-    char *s;
+	if (format == NULL)
+		return (-1);
+	output = init_buffer();
+	if (output == NULL)
+		return (-1);
 
-    while (*format != '\0')
-    {
-        if (*format == '%')
-        {
-            format++; // Move to the character after '%'
-            switch (*format)
-            {
-            case 'c':
-                c = va_arg(args, int);
-                putchar(c);
-                count++;
-                break;
-            case 's':
-                s = va_arg(args, char *);
-                while (*s != '\0')
-                {
-                    putchar(*s);
-                    s++;
-                    count++;
-                }
-                break;
-            case '%':
-                putchar('%');
-                count++;
-                break;
-            default:
-                // Unsupported conversion specifier, just print the '%'
-                putchar('%');
-                putchar(*format);
-                count += 2;
-                break;
-            }
-        }
-        else
-        {
-            putchar(*format);
-            count++;
-        }
-        format++;
-    }
+	va_start(args, format);
 
-    va_end(args);
+	ret = run_printf(format, args, output);
 
-    return count;
+	return (ret);
 }
